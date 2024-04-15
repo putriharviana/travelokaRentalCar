2daef53d90951903d77a931fbbfaf436565ecfb7
putriharviana <putriharviana@gmail.com>
2024-04-15 19:56:53 +0700

    Added code

--- cypress/e2e/UIAutomation.cy.js
+++
@@ -1,9 +0,0 @@
-describe ("Traveloka test cases", () => {
-    it("Flow traveloka action", () => {
-        cy.visit("https://zzzscore.com/1to50/en/");
-        for(let i=1 ; i<50; i++){
-            cy.get('[style*="opacity: 1"]').eq(i).its('length').should('eq', 1);
-            // findElementor.click();
-        }
-    })
-})
